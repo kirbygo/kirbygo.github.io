8d5b1eccc9e56ff5f3a18f4dd310a0545d181fb9
Daniel <kirbygo@hotmail.com>
2022-03-09 22:41:35 -0600

    x

--- js/resume.js
+++ js/resume.js
@@ -1,13 +1,25 @@
-!(function (t) {
-  "use strict";
-  t('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-      if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-          var e = t(this.hash);
-          if ((e = e.length ? e : t("[name=" + this.hash.slice(1) + "]")).length) return t("html, body").animate({ scrollTop: e.offset().top }, 1e3, "easeInOutExpo"), !1;
-      }
-  }),
-      t(".js-scroll-trigger").click(function () {
-          t(".navbar-collapse").collapse("hide");
-      }),
-      t("body").scrollspy({ target: "#sideNav" });
-})(jQuery);
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
